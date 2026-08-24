9bc5baa2997347276e61b2894162071f2e6c2ac8
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-24 21:50:56 +0530

    feat: add Joi validation middleware

---
+++ serverside/src/middleware/validate.middleware.js
@@ -0,0 +1,28 @@
+
+function isValid(schema) {
+
+    return (req, res, next) => {
+
+        const { error, value } = schema.validate(req.body,
+            {
+                abortEarly: false,
+                stripUnknown: true
+            });
+
+        // console.log('validate middleware:', this);
+
+        if (error) {
+            return res.status(400).json(
+                {
+                    message: 'Validation failed',
+                    error: error.details.map(detail => detail.message)
+                }
+            )
+        }
+        req.body = value;
+        next();
+    }
+
+}
+
+export default isValid;
