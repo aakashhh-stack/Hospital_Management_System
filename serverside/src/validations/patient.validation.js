8cd6bc63f686ebcac8f8dd55a0548af6c50d13f0
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-24 21:48:04 +0530

    feat: add patient validation schemas

---
+++ serverside/src/validations/patient.validation.js
@@ -0,0 +1,29 @@
+import Joi from 'joi'
+
+export const registerPatientSchema = Joi.object(
+    {
+        name: Joi.string().required().max(25),
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(8).max(12),
+        phone: Joi.string().required().pattern(/^[0-9]{10}$/),
+    }
+)
+
+
+export const loginPatientSchema = Joi.object(
+    {
+
+        email: Joi.string().required().email(),
+        password: Joi.string().required().min(8).max(12),
+
+    }
+)
+
+export const updatePatientSchema = Joi.object(
+    {
+        name: Joi.string().required().max(25),
+        email: Joi.string().required().email(),
+        phone: Joi.string().required().pattern(/^[0-9]{10}$/),
+
+    }
+).min(1)
