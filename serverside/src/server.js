1dfe988c35bd3565ef82a9e71784923debe40dfd
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-26 00:05:38 +0530

    feat(doctor): implement doctor registration API with validation

--- serverside/src/server.js
+++ serverside/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dbConnect from './config/connectDB.js'
 import dotenv from 'dotenv'
 import patientRoute from './routes/patient.routes.js'
+import doctorRoute from './routes/doctor.routes.js'
 dotenv.config()
 const app = express();
 
@@ -11,7 +12,8 @@ dbConnect();
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api',patientRoute);
+app.use('/api', patientRoute);
+app.use('/api', doctorRoute);
 
 app.listen(process.env.SERVER_PORT || 8080, () => {
     console.log(`server is started `)
