77a4a600a8710d214d75a88d92cdade39109bcbc
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-25 22:34:42 +0530

    feat:add authorization middleware

--- serverside/src/routes/patient.routes.js
+++ serverside/src/routes/patient.routes.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import isAuth from "../middleware/isAuth.js";
+import isValid from "../middleware/validate.middleware.js";
+import isAuthorized from '../middleware/role.middleware.js';
+import { registerPatientSchema, loginPatientSchema, updatePatientSchema } from '../validations/patient.validation.js';
 import { registerUser, loginUser, getPatientProfile, updatePatientProfile, deletePatientProfile } from '../controllers/patient.controllers.js';
 const router = express.Router();
 
-router.post('/patients/register', registerUser);
-router.post('/patients/login', loginUser);
+router.post('/patients/register', isValid(registerPatientSchema), registerUser);
+router.post('/patients/login', isValid(loginPatientSchema), loginUser);
 router.get('/patients/profile', isAuth, getPatientProfile);
-router.patch('/patients/profile', isAuth, updatePatientProfile);
+router.patch('/patients/profile', isAuth, isValid(updatePatientSchema), updatePatientProfile);
 router.delete('/patients/profile', isAuth, deletePatientProfile);
+
+
 
 export default router;
