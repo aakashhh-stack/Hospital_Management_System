674e6a2824dcd901021ef4ecf4199925e05f90f3
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-26 23:54:10 +0530

    feat:add appointment model

---
+++ serverside/src/models/appointment.model.js
@@ -0,0 +1,35 @@
+import { date, required } from "joi";
+import { Timestamp } from "mongodb";
+import mongoose from "mongoose";
+
+const appointmentSchema = mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Patient',
+        required: true
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true
+    },
+    appointmentDate: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['confirmed', 'pending', 'cancelled', 'completed'],
+        default: 'pending'
+    },
+    reason: {
+        type: String,
+        required: true,
+        maxLength: [35, 'Maximum 35 characters are allowed']
+    }
+
+}, { timestamps: true });
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+export default Appointment;
