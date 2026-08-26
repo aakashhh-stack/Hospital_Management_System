f3297755534947b6c24946bdb2d87852aa039863
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-27 00:04:44 +0530

    feat:add appointment validation schema

---
+++ serverside/src/validations/appointments.validation.js
@@ -0,0 +1,7 @@
+import Joi from 'joi';
+
+export const appointmentValidation = Joi.object({
+    doctor: Joi.string().required(),
+    appointmentDate: Joi.date().greater('now').required(),
+    reason: Joi.string().max(35).required()
+});
