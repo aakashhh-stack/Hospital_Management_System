5b1bd02892dea3bd22334820843bcc102a0d3f95
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-27 23:29:05 +0530

    feat: add appointment router middleware

--- serverside/src/server.js
+++ serverside/src/server.js
@@ -3,6 +3,8 @@ import dbConnect from './config/connectDB.js'
 import dotenv from 'dotenv'
 import patientRoute from './routes/patient.routes.js'
 import doctorRoute from './routes/doctor.routes.js'
+import appointmentRouter from './routes/appointment.routes.js'
+
 dotenv.config()
 const app = express();
 
@@ -14,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', patientRoute);
 app.use('/api', doctorRoute);
+app.use('/api', appointmentRouter);
 
 app.listen(process.env.SERVER_PORT || 8080, () => {
     console.log(`server is started `)
