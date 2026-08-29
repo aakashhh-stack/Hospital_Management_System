baf2e627da425d7fbc695b7ce046a8893c0d6949
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-29 23:18:05 +0530

    feat:add doctor retrieval endpoint

--- serverside/src/routes/doctor.routes.js
+++ serverside/src/routes/doctor.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import isAuth from '../middleware/isAuth.js';
 import isValid from '../middleware/validate.middleware.js';
 import isAuthorized from '../middleware/role.middleware.js';
-import { createDoctor, loginDoctor, updateDoctor, getDoctorProfile, deleteDoctor } from '../controllers/doctor.controller.js';
+import { createDoctor, loginDoctor, updateDoctor, getDoctorProfile, deleteDoctor,doctorAppointments } from '../controllers/doctor.controller.js';
 import { doctorRegistrationSchema, doctorLoginSchema, doctorUpdateSchema } from '../validations/doctor.validation.js';
 
 const router = express.Router();
@@ -12,4 +12,5 @@ router.post('/doctor/login', isValid(doctorLoginSchema), loginDoctor);
 router.patch('/doctor/profile', isAuth, isAuthorized('doctor'), isValid(doctorUpdateSchema), updateDoctor);
 router.get('/doctor/profile', isAuth, isAuthorized('doctor'), getDoctorProfile);
 router.delete('/doctor/profile',isAuth,isAuthorized('doctor'),deleteDoctor);
+router.get('/doctor/appointments',isAuth,isAuthorized('doctor'),doctorAppointments);
 export default router;
