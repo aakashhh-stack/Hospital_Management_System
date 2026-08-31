f5dc744c81fcfeb7c59b6f69bfb05fd783ca94cb
Aakash Pandey <tusharakapjack78@gmail.com>
2026-08-31 23:30:43 +0530

    feat:add admin doctor  management

--- serverside/src/routes/doctor.routes.js
+++ serverside/src/routes/doctor.routes.js
@@ -2,15 +2,59 @@ import express from 'express';
 import isAuth from '../middleware/isAuth.js';
 import isValid from '../middleware/validate.middleware.js';
 import isAuthorized from '../middleware/role.middleware.js';
-import { createDoctor, loginDoctor, updateDoctor, getDoctorProfile, deleteDoctor,doctorAppointments } from '../controllers/doctor.controller.js';
-import { doctorRegistrationSchema, doctorLoginSchema, doctorUpdateSchema } from '../validations/doctor.validation.js';
 
+// doctor controllers 
+import {
+    createDoctor, loginDoctor, updateDoctor,
+    getDoctorProfile, deleteDoctor, doctorAppointments,
+    getAllDoctors, updateDoctorStatus, deleteDoctorByAdmin
+} from '../controllers/doctor.controller.js';
+
+// doctor validations
+import {
+    doctorRegistrationSchema, doctorLoginSchema,
+    doctorUpdateSchema, doctorStatusUpdateSchema
+} from '../validations/doctor.validation.js';
+
+// create router
 const router = express.Router();
 
-router.post('/doctors/register', isValid(doctorRegistrationSchema), isAuth, isAuthorized('admin'), createDoctor);
+// -------------------------------- doctor routes ------------------------------------
+
+// ----------------------------- create doctor --------------------------------
+router.post('/doctors/register', isValid(doctorRegistrationSchema),
+    isAuth, isAuthorized('admin'), createDoctor);
+
+// ----------------------------- doctor login --------------------------------
 router.post('/doctor/login', isValid(doctorLoginSchema), loginDoctor);
-router.patch('/doctor/profile', isAuth, isAuthorized('doctor'), isValid(doctorUpdateSchema), updateDoctor);
+
+//----------------------------- Update doctor profile --------------------------------
+router.patch('/doctor/profile', isAuth, isAuthorized('doctor'),
+    isValid(doctorUpdateSchema), updateDoctor);
+
+//  ----------------------------- getdoctor profile --------------------------------
 router.get('/doctor/profile', isAuth, isAuthorized('doctor'), getDoctorProfile);
-router.delete('/doctor/profile',isAuth,isAuthorized('doctor'),deleteDoctor);
-router.get('/doctor/appointments',isAuth,isAuthorized('doctor'),doctorAppointments);
+
+//  ----------------------------- delete doctor profile --------------------------------
+router.delete('/doctor/profile', isAuth, isAuthorized('doctor'), deleteDoctor);
+
+//  ----------------------------- get doctor appointments --------------------------------
+router.get('/doctor/appointments', isAuth, isAuthorized('doctor'), doctorAppointments);
+
+
+// ---------------------------- admin doctor routes -----------------------------
+
+
+//  ----------------------------- admin get all doctors --------------------------------
+router.get('/admin/doctor', isAuth, isAuthorized('admin'), getAllDoctors);
+
+//  ----------------------------- admin update doctor status --------------------------------
+router.patch('/admin/doctor/:doctorId/status', isAuth,
+    isAuthorized('admin'), isValid(doctorStatusUpdateSchema), updateDoctorStatus);
+
+ //  ----------------------------- admin delete doctor profile --------------------------------   
+router.patch('/admin/doctor/:doctorId/delete', isAuth,
+    isAuthorized('admin'), deleteDoctorByAdmin);
+
+
 export default router;
