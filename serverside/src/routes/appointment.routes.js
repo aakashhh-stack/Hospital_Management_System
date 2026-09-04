0bf46b1f71d978b6a8d6f735c51df8d6b421ee9c
Aakash Pandey <tusharakapjack78@gmail.com>
2026-09-04 20:51:36 +0530

    feat:add admin appointment listing route

--- serverside/src/routes/appointment.routes.js
+++ serverside/src/routes/appointment.routes.js
@@ -6,15 +6,25 @@ import isValid from '../middleware/validate.middleware.js';
 import { appointmentValidation, updateAppointmentStatusValidation } from '../validations/appointments.validation.js';
 const router = express.Router();
 
+//------------------- Appointment routes -------------------
 router.post('/appointment',
     isAuth, isAuthorized('patient'),
     isValid(appointmentValidation),
     createAppointment
 );
 
+// ------------------- Doctor Update Appointment Status -------------------
+
 router.patch('/appointment/:appointmentId/status',
     isAuth, isAuthorized('doctor'),
     isValid(updateAppointmentStatusValidation),
     updateAppointmentStatus);
+
+// ------------------- Admin Get All Appointments -------------------
+router.get('/admin/appointments',
+    isAuth,
+    isAuthorized('admin'),
+    getAllAppointmentsByAdmin
+);
 
 export default router;
